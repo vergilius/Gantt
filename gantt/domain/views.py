bdf74f38e62fd54b3a66b9e83cb03660a0659267
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2014-04-28 21:52:37 +0200

    Added draft of login and main page.

--- gantt/domain/views.py
+++ gantt/domain/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login_required(func):
+
+    def inner(request):
+        if not request.user.is_authenticated():
+            return redirect('/login?next={}'.format(request.path))
+        else:
+            return func(request)
+
+    return inner
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+def login(request):
+    return render(request, 'login.html')
--- gantt/gantt/urls.py
+++ gantt/gantt/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+import domain.views
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'gantt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', domain.views.home),
+    url(r'^login/$', domain.views.login),
     url(r'^admin/', include(admin.site.urls)),
 )
